ec146f9718e1cbf536e5044254094d750f0fbe59
lso9@uwo.ca <lso9@uwo.ca>
2025-01-10 22:07:03 -0500

    Delete useless files

--- add-in/src/taskpane/components/App.jsx
+++ add-in/src/taskpane/components/App.jsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import Header from "./Header";
-import HeroList from "./HeroList";
-import TextInsertion from "./TextInsertion";
 import { makeStyles } from "@fluentui/react-components";
 import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 import { insertText } from "../taskpane";
@@ -18,15 +15,8 @@ const App = (props) => {
   const { title } = props;
   const styles = useStyles();
   
-
-
   return (
     <div className={styles.root}>
-      {/*
-      <Header logo="assets/logo-filled.png" title={title} message="Welcome" />
-      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
-      <TextInsertion insertText={insertText} />
-      */}
       <Header logo="assets/chaticon-80.png" title={title} message="Hi! I'm Fintelligent." />
       <ChatInterface />
     </div>
